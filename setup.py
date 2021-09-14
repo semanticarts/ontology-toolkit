d98adc0e48ee7ada49888dfe5bbb3ea83fa0f99f
Boris Pelakh <boris.pelakh@semanticarts.com>
2021-09-13 23:08:25 -0400

    Handle directories in copy and move.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="onto_tool",
-    version="1.4.1",
+    version="1.4.2",
     author="Boris Pelakh",
     author_email="boris.pelakh@semanticarts.com",
     description="Ontology Maintenance and Release Tool",
