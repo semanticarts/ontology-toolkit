5d986822716353156d8fd47bc40368761408830c
Boris Pelakh <pelakh@gmail.com>
2023-07-18 16:19:12 -0400

    Add markdown unit tests

--- onto_tool/mdutils.py
+++ onto_tool/mdutils.py
@@ -8,8 +8,6 @@ The class md2html() has a template that calls bootstrap for styling
 import jinja2
 import markdown2
 import io
-import shutil
-
 
 class Markdown2HTML:
 
@@ -55,39 +53,3 @@ class Markdown2HTML:
         doc = jinja2.Template(self.TEMPLATE).render(content=html)
         docfile = io.StringIO(doc)
         return docfile
-
-
-if __name__ == "__main__":
-    test = """Release notes gist 9.1.0
---------------------------
-
-* Reformatted all files to match uniform serialization.
-* Corrected restriction for `gist:Collection`
-* Provided missing labels for classes and properties.
-* Corrected issues [72](https://github.com/semanticarts/gist/issues/72),
-  [91](https://github.com/semanticarts/gist/issues/91), [95](https://github.com/semanticarts/gist/issues/95),
-  [96](https://github.com/semanticarts/gist/issues/96), [97](https://github.com/semanticarts/gist/issues/97),
-  [98](https://github.com/semanticarts/gist/issues/98), [101](https://github.com/semanticarts/gist/issues/101),
-  [122](https://github.com/semanticarts/gist/issues/122), and [145](https://github.com/semanticarts/gist/issues/145).
-* Removed outdated Visio and PDF files, documentation is now auto-generated as part of the release process.
-* gistWiki has been removed.
-
-Import URL: http://ontologies.semanticarts.com/o/gistCore9.1.0
-
-Release notes gist 9.0.0
---------------------------
-### General
-*	The `gist` namespace has been modfied from `http:` to `https:`.
-*	Added comments to ontologies.
-*	Added labels and comments to many properties and classes.
-*	`SocialBeing` has been removed.
-*	The property `gist:party` has been renamed to `gist:hasParty`.
-
-Import URL: http://ontologies.semanticarts.com/o/gistCore9.0.0
-"""
-    conv = Markdown2HTML()
-    outLocation = "./outFile.html"
-    f = conv.md2html(test)
-    with open(outLocation, 'w') as fd:
-        f.seek(0)
-        shutil.copyfileobj(f, fd)
