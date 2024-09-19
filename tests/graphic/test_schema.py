ff96e990b4fb63821ede7953a57adf0a1531b439
Kate Studzinski <kate.studzinski@semanticarts.com>
2024-09-19 15:38:42 -0400

    test_schema outdir pattern follows other tests

--- tests/graphic/test_schema.py
+++ tests/graphic/test_schema.py
@@ -3,17 +3,16 @@ import pydot
 
 
 def test_local_instance(tmp_path):
-    out_file = tmp_path / 'test_schema'
     onto_tool.main([
         'graphic',
         '-t', 'Local Ontology',
         '--no-image',
-        '-o', f'{out_file}',
+        '-o', f'{tmp_path}/test_schema',
         'tests/graphic/domain_ontology.ttl',
         'tests/graphic/upper_ontology.ttl',
         'tests/graphic/instance_data.ttl'
     ])
-    (instance_graph,) = pydot.graph_from_dot_file(f'{out_file}.dot')
+    (instance_graph,) = pydot.graph_from_dot_file(f'{tmp_path}/test_schema.dot')
     edges = list(sorted((e.get_source(), e.get_destination()) for e in instance_graph.get_edges()))
     assert edges == [
         ('Domain', 'Upper'),
