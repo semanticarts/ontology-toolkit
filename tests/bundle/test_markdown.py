676d590123cec969089ab45f9b2c7b08c8cd0ab8
Kate Studzinski <kate.studzinski@semanticarts.com>
2024-09-19 12:30:48 -0400

    Issue #133 Migrate test outputs to temp directory

--- tests/bundle/test_markdown.py
+++ tests/bundle/test_markdown.py
@@ -3,28 +3,28 @@ from glob import glob
 from os.path import basename
 
 
-def test_markdown_table():
+def test_markdown_table(tmp_path):
     onto_tool.main([
-        'bundle', '-v', 'output', 'tests-output/bundle',
+        'bundle', '-v', 'output', f'{tmp_path}',
         'tests/bundle/markdown.yaml'
     ])
 
-    html_text = open('tests-output/bundle/Table.html').read()
+    html_text = open(f'{tmp_path}/Table.html').read()
     assert '<table' in html_text.lower()
 
 
-def test_markdown_bulk():
+def test_markdown_bulk(tmp_path):
     onto_tool.main([
-        'bundle', '-v', 'output', 'tests-output/bundle',
+        'bundle', '-v', 'output', f'{tmp_path}',
         'tests/bundle/bulk_md.yaml'
     ])
 
-    inc_no_exc = [basename(f) for f in glob('tests-output/bundle/bulk_md/inc_no_exc/*')]
+    inc_no_exc = [basename(f) for f in glob(f'{tmp_path}/bulk_md/inc_no_exc/*')]
     assert sorted(inc_no_exc) == ['a1.html']
 
-    exc_no_inc = [basename(f) for f in glob('tests-output/bundle/bulk_md/exc_no_inc/*')]
+    exc_no_inc = [basename(f) for f in glob(f'{tmp_path}/bulk_md/exc_no_inc/*')]
     assert sorted(exc_no_inc) == ['a1.html', 'c3.html']
 
-    inc_and_exc = [basename(f) for f in glob('tests-output/bundle/bulk_md/inc_and_exc/*')]
+    inc_and_exc = [basename(f) for f in glob(f'{tmp_path}/bulk_md/inc_and_exc/*')]
     assert sorted(inc_and_exc) == ['b2.html', 'c3.html']
 
--- tests/graphic/__init__.py
+++ tests/graphic/__init__.py
@@ -1,3 +1,1 @@
-import os
-if not os.path.isdir('tests-output/graphic'):
-    os.makedirs('tests-output/graphic')
+
